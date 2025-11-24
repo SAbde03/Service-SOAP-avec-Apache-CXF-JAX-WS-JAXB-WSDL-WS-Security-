37b31b14ed99a629f6e4de02af327a4fd5d13dba
SAbde03 <salhiabde03@gmail.com>
2025-11-24 23:46:48 +0100

    first commit

---
+++ src/main/java/com/acme/cxf/App.java
@@ -0,0 +1,20 @@
+package com.acme.cxf;
+
+import com.acme.cxf.impl.HelloServiceImpl;
+import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        String address = "http://localhost:8080/services/hello";
+        JaxWsServerFactoryBean factory = new JaxWsServerFactoryBean();
+        factory.setServiceClass(HelloServiceImpl.class);
+        factory.setAddress(address);
+        factory.create();
+        System.out.println("WSDL: " + address + "?wsdl");
+    }
+}
